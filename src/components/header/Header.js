10f346b46964319bdd3a2353bd58e722258a1148
Viper4717 <aahad.24csedu.049@gmail.com>
2020-08-16 15:51:15 +0600

    header styling modified

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -4,11 +4,11 @@ import { Navbar, Nav } from 'react-bootstrap';
 function Header() {
   return (
     <div>
-        <Navbar className="shadow-lg p-6 mb-5" bg="primary" variant="dark" expand="lg" sticky="top">
-            <Navbar.Brand href="#home">Nilkhetian</Navbar.Brand>
+        <Navbar className="shadow-lg p-1 mb-5" bg="primary" variant="dark" expand="lg" sticky="top">
+            <Navbar.Brand href="#home" style={{paddingLeft: "15px"}}>Nilkhetian</Navbar.Brand>
             <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-              <Navbar.Text>
+              <Navbar.Text style={{paddingRight: "15px"}}>
                 <Nav className="mr-auto">
                     <Nav.Link href="#home">Home</Nav.Link>
                     <Nav.Link href="#home">Link</Nav.Link>
