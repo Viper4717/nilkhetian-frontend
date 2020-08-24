2e28b80b54cb0c9faf218baa8152a37254b27951
Viper4717 <aahad.24csedu.049@gmail.com>
2020-08-24 20:38:07 +0600

    Website header design modified

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,50 +1,50 @@
 import React from 'react';
-import { Navbar, Nav, Form, FormControl, Button } from 'react-bootstrap';
+import { Navbar, Nav, Form, FormControl, Button} from 'react-bootstrap';
 import HeaderLogo from '../../assets/nilkhetianLogoHeader.png'
+import MobileHeaderLogo from '../../assets/nilkhetianMobileLogoHeader.svg'
 import { CgProfile, CgSearch, CgShoppingCart } from 'react-icons/cg';
 import './Header.css';
 
 function Header() {
   return (
     <div>
-      <Navbar className="shadow-sm p-1 mb-5" expand="lg" sticky="top">
-          <Navbar.Brand href="#home">
+      <Navbar className="shadow-sm p-1 justify-content-between" expand="md" sticky="top">
+        <Navbar.Brand className="webBrand" href="#home">
           <img
             src={HeaderLogo}
             width="180"
             height="40"
-            className="d-inline-block align-top"
             alt="Nilkhetian Logo"
           />
         </Navbar.Brand>
+        <Navbar.Brand className="mobileBrand" href="#home">
+          <img
+            src={MobileHeaderLogo}
+            width="50"
+            height="50"
+            alt="Nilkhetian Mobile Logo"
+          />
+        </Navbar.Brand>
         <Form inline>
-          <FormControl type="text" placeholder="Search Products" className="mr-sm-2" />
-          {/*<><style type="text/css">
-            {`
-              .btn-custom {
-                background-color: #ffa500;
-                color: white;
-              }
-            `}
-          </style>*/}
-          <Button variant="custom" size='sm'>
-            <CgSearch size='2em'/>
-          </Button>
+          <div style={{display: "flex", flexDirection: "row"}}>
+            <FormControl type="text" placeholder="Search Products" className="mr-sm-2" />
+            <Button variant="custom" size='sm'>
+              <CgSearch size='2em'/>
+            </Button>
+          </div>
         </Form>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
-          <Navbar.Text>
-            <Nav className="mr-auto">
-                <Nav.Link href="#home">
-                  <CgProfile size='2em'/>
-                  <h5 className="iconText">Profile</h5>
-                </Nav.Link>
-                <Nav.Link href="#home">
-                  <CgShoppingCart size='2em'/>
-                  <h5 className="iconText">Cart</h5>
-                </Nav.Link>
-            </Nav>
-          </Navbar.Text>
+        <Navbar.Collapse id="basic-navbar-nav" >
+          <Nav>
+            <Nav.Link href="#home">
+              <CgProfile size='2em'/>
+              <h5 className="iconText">Profile</h5>
+            </Nav.Link>
+            <Nav.Link href="#home">
+              <CgShoppingCart size='2em'/>
+              <h5 className="iconText">Cart</h5>
+            </Nav.Link>
+          </Nav>
         </Navbar.Collapse>
       </Navbar>
     </div>
