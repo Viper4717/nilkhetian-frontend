921f4755080d16bba62de55bb0322b563379c2bb
Viper4717 <aahad.24csedu.049@gmail.com>
2020-08-25 15:47:27 +0600

    Homepage banner modified with overlay

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import './Home.css'
 import WebsiteBanner from '../../assets/home/websiteBanner.jpg'
-import { Button, Container } from 'react-bootstrap';
+import { Button, Container, Card } from 'react-bootstrap';
 
 function Home() {
   return (
     <div>
-        <Container>
-        <img className="siteBanner"
-            src={WebsiteBanner}
-            alt="Website Banner"
-        />
-        <h2 className="siteHeading">
-            Nilkhetian is an online platform that gives you the original 
-            Nilkhet experience from the comfort of your home.
-        </h2>
-        <Button variant="primary">
-            Explore our Library
-        </Button>
-        </Container>
+      <Container>
+        <Card className="siteBanner">
+          <Card.Img className="bannerImage" src={WebsiteBanner} alt="Site Banner"/>
+            <Card.ImgOverlay className="bannerOverlay">
+              <Card.Title className="bannerTitle">
+                  Welcome to Nilkhetian
+              </Card.Title>
+              <Card.Text className="bannerText">
+                An online platform that gives you
+                the original Nilkhet experience from
+                the comfort of your Home.
+              </Card.Text>
+            </Card.ImgOverlay>
+        </Card>
+      </Container>
     </div>
   );
 }
