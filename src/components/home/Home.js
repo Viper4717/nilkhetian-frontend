795c45905fe2ebbf6620a31ec1e525db4061db6a
Viper4717 <aahad.24csedu.049@gmail.com>
2020-08-26 16:31:18 +0600

    header menu fonts modified

--- src/components/home/Home.js
+++ src/components/home/Home.js
@@ -11,21 +11,21 @@ function Home() {
         <img className="mobileTopBanner" src={MobileTopBanner}/>
         <Card className="siteBanner">
           <Card.Img className="bannerImage" src={WebsiteBanner} alt="Site Banner"/>
-            <Card.ImgOverlay className="bannerOverlay">
-              <Card.Title className="bannerTitle">
-                  Welcome to Nilkhetian
-              </Card.Title>
-              <Card.Text className="bannerText">
-                An online platform that gives you
-                the original Nilkhet experience from
-                the comfort of your Home.
-              </Card.Text>
-            </Card.ImgOverlay>
-            <Card.ImgOverlay className="buttonOverlay">
-              <Button className="exploreButtonWeb" variant="custom">
-                EXPLORE NOW
-              </Button>
-            </Card.ImgOverlay>
+          <Card.ImgOverlay className="bannerOverlay">
+            <Card.Title className="bannerTitle">
+                Welcome to Nilkhetian
+            </Card.Title>
+            <Card.Text className="bannerText">
+              An online platform that gives you
+              the original Nilkhet experience from
+              the comfort of your Home.
+            </Card.Text>
+          </Card.ImgOverlay>
+          <Card.ImgOverlay className="buttonOverlay">
+            <Button className="exploreButtonWeb" variant="custom">
+              EXPLORE NOW
+            </Button>
+          </Card.ImgOverlay>
         </Card>
         <Button className="exploreButtonMobile" variant="custom">
           EXPLORE NOW
