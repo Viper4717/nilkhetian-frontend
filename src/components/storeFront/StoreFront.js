87def479a18876abf92b5da43c2b1dc0308130b3
Viper4717 <aahad.24csedu.049@gmail.com>
2020-09-02 16:19:42 +0600

    store front web grid created

--- src/components/storeFront/StoreFront.js
+++ src/components/storeFront/StoreFront.js
@@ -4,35 +4,38 @@ import BookStoreImage from '../../assets/bookStore/bookStore.jpg'
 import { Container, Button, Row, Card } from 'react-bootstrap';
 
 function StoreFront() {
-  return (
-    <Container fluid="md" className="parentContainer smallHeight padBottomContainer">
-        <h2 className="storeHeader"> Nilkhet Online: Store Front </h2>
-        <div className="topButtonDiv">
-            <Button className="bookCategoryButton" variant="custom">
-                Search by Book Categories
-            </Button>
-        </div>
-        <Row>
-            <Card className="storeCard">
-                <div className="storeCardDiv">
-                    <Card.Title className="storeTitle">
-                        Book Store Name
-                    </Card.Title>
-                    <div className="storeCoverBg">
-                        <Card.Img className="storeCover" src={BookStoreImage} alt="Book Image"/>
-                    </div>
-                    <div className="storeDetailsBg">
-                        <Card.Text className="storeDetails">
-                            This is where my friend, the details, the unique details
-                            of the store, will be shown, proudly, gloriously.
-                        </Card.Text>
-                    </div>
-                    <Button className="visitStoreButton" variant="custom"> Visit This Store </Button>
-                </div>
-            </Card>
-        </Row>
-    </Container>
-  );
+    const testArray=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    return (
+        <Container fluid="md" className="parentContainer smallHeight padBottomContainer">
+            <h2 className="storeHeader"> Nilkhet Online: Store Front </h2>
+            <div className="topButtonDiv">
+                <Button className="bookCategoryButton" variant="custom">
+                    Search by Book Categories
+                </Button>
+            </div>
+            <div className="storeGrid">
+                {testArray.map(index => (
+                    <Card className="storeCard">
+                        <div className="storeCardDiv">
+                            <Card.Title className="storeTitle">
+                                Book Store Name
+                            </Card.Title>
+                            <div className="storeCoverBg">
+                                <Card.Img className="storeCover" src={BookStoreImage} alt="Book Image"/>
+                            </div>
+                            <div className="storeDetailsBg">
+                                <Card.Text className="storeDetails">
+                                    This is where my friend, the details, the unique details
+                                    of the store, will be shown, proudly, gloriously.
+                                </Card.Text>
+                            </div>
+                            <Button className="visitStoreButton" variant="custom"> Visit This Store </Button>
+                        </div>
+                    </Card>
+                ))}
+            </div>
+        </Container>
+    );
 }
 
 export default StoreFront;
