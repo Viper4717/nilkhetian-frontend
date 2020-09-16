037c100d3215defebf40c264a74de705ae9f05f1
Viper4717 <aahad.24csedu.049@gmail.com>
2020-09-16 18:22:13 +0600

    url encoding implemented

--- src/util.js
+++ src/util.js
@@ -1,12 +1,30 @@
 const serverUrl = process.env.REACT_APP_SERVER_URL || 'http://118.179.145.125:25565';
 
 function categoryParse(item){
-    const catName = item.replace(" ", "+");
+    var catName = item.replace(" ", "+");
+    catName = catName.replace("'", "%27");
+    catName = catName.replace("\"", "%22");
+    catName = catName.replace("&", "%26");
+    catName = catName.replace(",", "%2C");
+    catName = catName.replace("-", "%2D");
+    catName = catName.replace(":", "%3A");
+    catName = catName.replace(";", "%3B");
+    catName = catName.replace("?", "%3F");
+
     return catName;
 }
 
 function reverseCategoryParse(item){
-    const catName = item.replace("+", " ");
+    var catName = item.replace("+", " ");
+    catName = catName.replace("%27", "'");
+    catName = catName.replace("%22", "\"");
+    catName = catName.replace("%26", "&");
+    catName = catName.replace("%2C", ",");
+    catName = catName.replace("%2D", "-");
+    catName = catName.replace("%3A", ":");
+    catName = catName.replace("%3B", ";");
+    catName = catName.replace("%3F", "?");
+
     return catName;
 }
 
