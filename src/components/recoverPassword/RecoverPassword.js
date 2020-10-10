672217fa8248d330d5a05468dffbe0fb5142c782
A.M. Aahad <45429535+Viper4717@users.noreply.github.com>
2020-10-10 20:57:06 +0600

    password recovery implemented

--- src/components/recoverPassword/RecoverPassword.js
+++ src/components/recoverPassword/RecoverPassword.js
@@ -3,10 +3,10 @@ import { Form, Button, Container, Alert, Spinner } from 'react-bootstrap';
 import Axios from 'axios';
 import { serverUrl } from '../../util';
 
-function changeUserPass(passwordObject, setLoading, setError){
-    Axios.post(`${serverUrl}/api/user/resendconfirmation`, passwordObject)
+function changeUserPass(passwordObject, token, setLoading, setError){
+    Axios.post(`${serverUrl}/api/user/resetpassword/${token}`, passwordObject)
     .then(({data: res}) => {
-        window.location.assign('/recoverpassword/success');
+        window.location.assign('/recoverpasswordsuccess');
     })
     .catch((error) => {
         window.scrollTo(0, 0);
@@ -16,20 +16,30 @@ function changeUserPass(passwordObject, setLoading, setError){
     });
 }
 
+function validateEmail(email) {
+    const re = /\S+@\S+\.\S+/
+    return re.test(email);
+}
+
 function RecoverPassword() {
 
+    const [token, setToken] = useState();
     const [error, setError] = useState();
     const [loading, setLoading] = useState(false);
     const [password, setPassword] = useState();
     const [confirmPass, setConfirmPass] = useState();
+    const [email, setEmail] = useState();
 
     useEffect(() => {
         window.scrollTo(0, 0);
-        const code = window.location.href.substring(
+        const newToken = window.location.href.substring(
             window.location.href.indexOf("/recoverpassword")+17, window.location.href.length);
-        if(code == ""){
+        if(newToken == ""){
             const err = "Invalid token.";
             setError(err);
+        }
+        else{
+            setToken(newToken);
         }
     }, [])
 
@@ -39,12 +49,15 @@ function RecoverPassword() {
     function handleConfirmPassword(e){
         setConfirmPass(e.target.value);
     }
+    function handleEmail(e){
+        setEmail(e.target.value);
+    }
 
     function changePass(e){
         e.preventDefault();
-        if(password && confirmPass){
+        if(password && confirmPass && email){
             setError(null);
-            if(password.length < 8 || password !== confirmPass){
+            if(password.length < 8 || password !== confirmPass || !validateEmail(email)){
                 window.scrollTo(0, 0);
                 const err = "Invalid data."
                 setError(err);
@@ -53,14 +66,15 @@ function RecoverPassword() {
                 setError(null);
                 setLoading(true);
                 const passwordObject = {
-                    password: password,
+                    newPassword: password,
+                    email: email,
                 }
-                changeUserPass(passwordObject, setLoading, setError);
+                changeUserPass(passwordObject, token, setLoading, setError);
             }
         }
         else{
             window.scrollTo(0, 0);
-            const err = "Please fill out both fields."
+            const err = "Please fill out all the fields."
             setError(err);
         }
     }
@@ -91,6 +105,14 @@ function RecoverPassword() {
                         onChange={handleConfirmPassword}/>
                         {(confirmPass && password !== confirmPass) &&
                         <Form.Text className="passwordWarnText"> Passwords don't match! </Form.Text>}
+                    </Form.Group>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label>Confirm you e-mail address</Form.Label>
+                        <Form.Control type="email" placeholder="Enter email" onChange={handleEmail}/>
+                        {(email && !validateEmail(email)) &&
+                        <Form.Text className="passwordWarnText">
+                            Enter a valid e-mail address.
+                        </Form.Text>}
                     </Form.Group>
                 </div>
                 <div className="signInButtonOverlay">
